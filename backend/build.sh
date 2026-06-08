a7d51f9435e5167bf4fc9283b811abb2c46bb08d
Swarup Mandal <swarupmandal630@gmail.com>
2026-06-08 17:19:43 +0530

    add superuser to build

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+python manage.py createsuperuser \
+  --noinput \
+  --username admin \
+  --email findmydoc@gmail.com|| true
